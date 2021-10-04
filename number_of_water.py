6d13b2a90ef9f26cacfd69e801d255941bb9fcb6
seyed mohammad kazemm manzoorolajdad <kazem.manzoor@gmail.com>
2021-10-04 13:59:49 +0330

    Add files via upload
    calcute number of water molecule

---
+++ number_of_water.py
@@ -0,0 +1,7 @@
+lx=float(input("please insert lx= ")) #43.07680
+ly=float(input("please insert ly= ")) #43.76300
+lz=float(input("please insert lz= ")) #50.00
+
+x=(6.02*10**23)*lx*ly*lz*(10**(-24))/18.05
+
+print("number of water:" , x)
