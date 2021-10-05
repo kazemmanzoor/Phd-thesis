f5a0f123ee33a582cbefb6b91556f0e29452ce1a
seyed mohammad kazemm manzoorolajdad <kazem.manzoor@gmail.com>
2021-10-05 08:08:17 +0330

    Update post-script.sh
    add line for NA & CL ions

--- post-script.sh
+++ post-script.sh
@@ -1,8 +1,12 @@
 
-tail H-blue-0na-300K.density -n 1500 >ready-H-blue-0na-300K.density
+tail H-blue-0na-300K-0x-0y.density -n 1500 >ready-H-blue-0na-300K-0x-0y.density
 
-tail O-blue-0na-300K.density -n 1500 >ready-O-blue-0na-300K.density
+tail O-blue-0na-300K-0x-0y.density -n 1500 >ready-O-blue-0na-300K-0x-0y.density
 
-tail WATER-blue-0na-300K.density -n 1500 >ready-WATER-blue-0na-300K.density
+tail WATER-blue-0na-300K-0x-0y.density -n 1500 >ready-WATER-blue-0na-300K-0x-0y.density
+
+tail NA-blue-0na-300K-0x-0y.density -n 1500 >ready-NA-blue-0na-300K-0x-0y.density
+
+tail CL-blue-0na-300K-0x-0y.density -n 1500 >ready-CL-blue-0na-300K-0x-0y.density
 
 python3 plot-density.py
